66fa4b4508a80055cd26cb0b3c605d11ab8f9e53
AryanSachan12 <aryansachan2004@gmail.com>
2025-04-11 13:26:39 +0530

    Call feature

---
+++ backend/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from gtts import gTTS
+from routes.patient import router as patientRouter
+from routes.twilio import router as twilioRouter
+from routes.doctor import router as doctorRouter
+from routes.report import router as reportRouter
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+app.include_router(patientRouter)
+app.include_router(twilioRouter)
+app.include_router(doctorRouter)
+app.include_router(reportRouter)
